8d65d40c07ff7ea9efa03bdb89efb9d719e33eac
rkeene <rkeene@noemail.net>
2009-03-18 19:03:57 +0000

    Updated Rivet parsing
    FossilOrigin-Name: cedd9832ecdfdd084f14e7ae2f6b9967c92b2315

--- rivet-starkit/main.tcl
+++ rivet-starkit/main.tcl
@@ -58,6 +58,7 @@ if {![file exists $targetfile]} {
 # Determine what to do with the file based on its filename
 switch -glob -- $targetfile {
 	"*.rvt" {
+		cd [file dirname $targetfile]
 		parse $targetfile	
 	}
 	"*.png" {
