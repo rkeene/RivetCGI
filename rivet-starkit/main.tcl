fa65c3ab30d81d7c7474ea74726418fa2278b3ac
rkeene <rkeene@noemail.net>
2009-04-11 22:33:15 +0000

    Fixed print_help definition
    FossilOrigin-Name: ed4a45e4eac082df11eba6fd89a6dc628390ebe6

--- rivet-starkit/main.tcl
+++ rivet-starkit/main.tcl
@@ -628,7 +628,7 @@ proc call_page {} {
 	}
 }
 
-proc print_help {
+proc print_help {} {
 	tcl_puts "Usage: [file tail $argv0] {--server \[--address <address>\] \[--port <port>\]"
 	tcl_puts "                       \[--foreground {yes|no}\]|--cgi|--help|--version}"
 	tcl_puts "   --server           Run in standalone server mode"
