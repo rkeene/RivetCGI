d2a544c9b1d7f0a6ea89a0c548ecee9951d87def
rkeene <rkeene@noemail.net>
2009-04-11 22:00:14 +0000

    Fixed unbalanced close-brace in comment
    FossilOrigin-Name: 77cace3e187081bd6fa7fe7e008122a3739e075c

--- rivet-starkit/main.tcl
+++ rivet-starkit/main.tcl
@@ -89,7 +89,7 @@ proc call_page {} {
 	}
 	
 	# Determine what to do with the file based on its filename
-	## cat /etc/httpd/mime.types | sed 's@#.*$@@' | while read mt exts; do if [ -z "${exts}" ]; then continue; fi; cb=""; for ext in $exts; do cb="${cb} \"*.${ext}\" - "; done; cb="${cb} { set statictype \"$mt\" }"; echo $cb; done | grep -v 'application/octet-stream' | sed 's@ - {@ {@' | sed 's@^@\t@'
+	## cat /etc/httpd/mime.types | sed 's@#.*$@@' | while read mt exts; do if [ -z "${exts}" ]; then continue; fi; cb=""; for ext in $exts; do cb="${cb} \"*.${ext}\" - "; done; cb="${cb} { set statictype \"$mt\" }"; echo $cb; done | grep -v 'application/octet-stream' | sed 's@ - {@ {@' | sed 's@^@\t@'; # }
 	switch -glob -- [string tolower $targetfile] {
 		"*.rvt" {
 			package require tclrivet
